ebc6fe5279dfbd444618b88f3ee038ab39bc2908
JC Alemis <juancruzalemis@gmail.com>
2025-05-09 23:57:32 -0300

    subiendo tp 05

---
+++ 05 Funciones/Practico 5 Listas.py
@@ -0,0 +1,12 @@
+#1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función range.
+
+multiplos_de_4 = list(range(4, 101, 4))
+print(multiplos_de_4)
+
+#2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo. ¡Puedes hacerlo como se muestra 
+# en los videos o bien investigar cómo funciona el indexing con números negativos! 3) Crear una lista vacía, agregar tres palabras
+
+mis_favoritos = ["Futbol", "F1", "Tenis", "Música", "Café"]
+penultimo = mis_favoritos[-2]
+print(penultimo)
+
