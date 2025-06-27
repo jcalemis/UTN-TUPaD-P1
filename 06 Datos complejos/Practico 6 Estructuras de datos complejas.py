b80bd7d773e131497737e99ce396f946ebb30f85
JC Alemis <juancruzalemis@gmail.com>
2025-06-27 20:21:38 -0300

    Subiendo tp 6

---
+++ 06 Datos complejos/Practico 6 Estructuras de datos complejas.py
@@ -0,0 +1,33 @@
+#1) Dado el diccionario precios_frutas 
+precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
+1450}
+
+
+precios_frutas["Naranja"] = "1200"
+precios_frutas["Manzana"] = "1500"
+precios_frutas["Pera"] = "2300"
+
+
+print(precios_frutas)
+
+#2) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
+#desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:
+
+precios_frutas["Banana"] = "1330"
+precios_frutas["Manzana"] = "1700"
+precios_frutas["Melón"] = "2800"
+
+
+print(precios_frutas)
+
+
+#3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
+#desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los
+#precios.
+
+lista_frutas=["Banana","Ananá","Melón","Uva","Naranja","Manzana","Pera"]
+
+
+print(lista_frutas)
+
+
